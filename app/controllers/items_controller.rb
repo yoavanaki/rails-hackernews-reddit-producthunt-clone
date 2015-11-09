71c833e22abcf1a5e595db1bd3b03fd3d7dd0572
yoav <yoavanaki@gmail.com>
2015-11-09 09:59:46 +0200

    small fixes

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,7 +13,7 @@ class ItemsController < ApplicationController
     end  
     @items = @items.group_by {|c| c.created_at.to_date() }
      # @items.order(created_at: :desc)
- #     @items.keys.sort
+      @items.keys.sort
   end
 
   def show
