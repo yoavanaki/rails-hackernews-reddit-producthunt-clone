19d925bfef14a8d462213d2bef9503ae4c8942e7
yoav <yoavanaki@gmail.com>
2015-11-09 09:50:09 +0200

    another try

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,7 +12,7 @@ class ItemsController < ApplicationController
       object[item.id] = item.votes.map(&:user_id)
     end  
     @items = @items.group_by {|c| c.created_at.to_date() }
-      @items.order(keys: :desc)
+      @items.order(created_at: :desc)
  #     @items.keys.sort
   end
 
